14274afee40be7fed77effe4c4120a73fb064dca
Xi Xiao <xi.xiao@biit.fi>
2018-03-09 23:47:29 +0200

    +

--- bash-setup.sh
+++ bash-setup.sh
@@ -17,13 +17,15 @@ ${home_path}/dotfiles/bootstrap.sh -f
 /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
 sudo dpkg -i ./keyring.deb
 echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+
 # for java8
 sudo add-apt-repository ppa:webupd8team/java -y
 
 # add enpass source
 echo "deb http://repo.sinew.in/ stable main" | sudo tee --append /etc/apt/sources.list.d/enpass.list
 wget -O - https://dl.sinew.in/keys/enpass-linux.key | sudo apt-key add -
-sudo apt-add-repository ppa:fish-shell/release-2 -y
+
+sudo add-apt-repository ppa:fish-shell/release-2 -y
 
 # refresh sudo apt-get update
 sudo apt-get update -y
