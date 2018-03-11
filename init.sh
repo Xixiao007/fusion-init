33f7ee7fec93336aab13fe220fe6e0bc54fa0213
Xi Xiao <xi.xiao007@gmail.com>
2018-03-11 11:47:15 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,9 +16,20 @@ sudo apt-get install software-properties-common -y
 sudo dpkg -i ./keyring.deb
 echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> sudo tee --append /etc/apt/sources.list.d/sur5r-i3.list
 
+# vm tools
+sudo apt-get install open-vm-tools-desktop -y
+
+# copy keys from host
+mkdir ${home_path}/Shared
+/usr/bin/vmhgfs-fuse -o auto_unmount .host:/ ${home_path}/Shared
+mkdir ${home_path}/.ssh
+cp ${home_path}/Shared/${hostusername}/.ssh/id_rsa ${home_path}/.ssh/
+cp ${home_path}/Shared/${hostusername}/.ssh/id_rsa.pub ${home_path}/.ssh/
+chmod og-rw ${home_path}/.ssh/id_rsa
+
 # dotfiles
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ${home_path}/.ssh/config
-git clone https://github.com/Xixiao007/dotfiles.git ${home_path}/dotfiles
+git clone git@github.com:Xixiao007/dotfiles.git ${home_path}/dotfiles
 ${home_path}/dotfiles/bootstrap.sh -f
 
 # add enpass source
