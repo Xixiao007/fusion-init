c3657dacacd93b5255afb374a29e38cdf568aa84
Xi Xiao <xi.xiao007@gmail.com>
2018-03-11 09:56:28 +0200

    Update vmware-tools.sh

--- vmware-tools.sh
+++ vmware-tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo mkdir /mnt/too/
+sudo mkdir /mnt/tool/
 sudo mount /dev/sr0 /mnt/tool
-cp -r /mnt/tool/* ~
-tar zxpf ~/VMwareTools*
-cd ~/VMwareTools*
+cp -r /mnt/tool/* .
+tar zxpf VMwareTools*
+cd vmware-tools*
 sudo ./vmware-install.pl
