c7352a5ce67fa57666d145d19e38ddca920c6360
Xi Xiao <xi.xiao007@gmail.com>
2018-03-11 12:25:20 +0200

    Update post_steps.sh

--- post_steps.sh
+++ post_steps.sh
@@ -4,6 +4,23 @@
 username=finxxi
 home_path=/home/${username}
 
+
+# vm tools
+sudo apt-get install open-vm-tools-desktop -y
+
+# copy keys from host
+mkdir ${home_path}/Shared
+/usr/bin/vmhgfs-fuse -o auto_unmount .host:/ ${home_path}/Shared
+mkdir ${home_path}/.ssh
+cp ${home_path}/Shared/${hostusername}/.ssh/id_rsa ${home_path}/.ssh/
+cp ${home_path}/Shared/${hostusername}/.ssh/id_rsa.pub ${home_path}/.ssh/
+chmod og-rw ${home_path}/.ssh/id_rsa
+
+# dotfiles
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ${home_path}/.ssh/config
+git clone git@github.com:Xixiao007/dotfiles.git ${home_path}/dotfiles
+${home_path}/dotfiles/bootstrap.sh -f
+
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git /home/${username}/.fzf
 /home/${username}/.fzf/install
