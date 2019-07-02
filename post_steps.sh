222894f592c8dd883836caad171854e2345e414a
Xi Xiao <xi.xiao@biit.fi>
2019-07-02 23:39:04 +0300

    Update post_steps.sh

--- post_steps.sh
+++ post_steps.sh
@@ -29,13 +29,13 @@ sudo apt-get install open-vm-tools-desktop -y
 # eval {$home_path}/dotfiles/bootstrap.sh -f
 
 # for java8
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update -y
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-  sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer -y
+#sudo add-apt-repository ppa:webupd8team/java -y
+#sudo apt-get update -y
+#echo debconf shared/accepted-oracle-license-v1-1 select true | \
+#  sudo debconf-set-selections
+#echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+#  sudo debconf-set-selections
+#sudo apt-get install oracle-java8-installer -y
 
 # font Consolas
 sudo apt-get install font-manager cabextract -y
