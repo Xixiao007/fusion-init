191e6c56ce5fdef9cc1a5396ad17221a7567d031
Xi Xiao <xi.xiao007@gmail.com>
2019-07-13 21:20:50 +0300

    Update post_steps.sh

--- post_steps.sh
+++ post_steps.sh
@@ -8,6 +8,14 @@ set temp_path {$home_path}/temp
 mkdir -p {$home_path}/temp
 cd {$temp_path}
 
+# brave browser
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+source /etc/os-release
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
+sudo apt update
+sudo apt install brave-keyring brave-browser -y
+
+
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git /home/{$username}/.fzf
 eval /home/{$username}/.fzf/install
